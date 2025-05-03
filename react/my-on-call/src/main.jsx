878f35511b10b359a0f5ff52e92b5ddae85fde73
Steven <shelesteven@gmail.com>
2025-05-03 10:34:18 -0400

    Added basic routing

--- react/my-on-call/src/main.jsx
+++ react/my-on-call/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import "./index.css";
+
+const root = document.getElementById("root");
+
+createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/test" element={<div>Test</div>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
