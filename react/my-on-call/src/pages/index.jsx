ed94abd14245ce19e8ecdfa02b0672b7c0f28b21
Steven <shelesteven@gmail.com>
2025-05-03 10:59:34 -0400

    Styling changes

--- react/my-on-call/src/pages/index.jsx
+++ react/my-on-call/src/pages/index.jsx
@@ -4,8 +4,10 @@ export default function Index() {
   return (
     <>
       <title>Home - My On Call</title>
-      <h1 class="foreground-primary">My On Call</h1>
-      <h2 class="foreground-accent">A simple on-call schedule</h2>
+      <div class="section">
+        <h1 class="foreground-primary inter">My On Call</h1>
+        <h2 class="foreground-accent quicksand">A simple on-call schedule</h2>
+      </div>
     </>
   );
 }
